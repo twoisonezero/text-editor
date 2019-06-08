5f157d3fcbde8d6276b6c83112c7ef5acb4482b1
TiZO Admin <admin@twoisonezero.com>
2019-06-07 19:07:10 -0500

    initial commit

---
+++ main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <termbox.h>
+
+struct key {
+	unsigned char x;
+	unsigned char y;
+	uint32_t ch;
+};
+
+#define STOP {0,0,0}
+struct key K_ESC[] = {{1,1,'E'},{2,1,'S'},{3,1,'C'},STOP};
+
+void draw_key(struct key *k, uint16_t fg, uint16_t bg)
+{
+	while (k->x)
+	{
+		tb_change_cell(k->x+2, k->y+4, k->ch, fg, bg);
+		k++;
+	}
+}
+
+int main()
+{
+	printf("Hello world\n");
+	tb_init();
+
+	tb_select_input_mode(TB_INPUT_ESC | TB_INPUT_MOUSE);
+	tb_select_output_mode(TB_OUTPUT_256);
+
+	tb_clear();
+
+	const char* myName = "steel";
+
+	int length = strlen(myName);
+	for (int i = 0; i < length; i++)
+	{
+		tb_change_cell(i, 0, myName[i], 32+i, 231);
+	}
+
+	draw_key(K_ESC, TB_WHITE, TB_BLUE);
+	tb_present();
+
+	for (;;)
+	{
+		struct tb_event ev;
+		int t = tb_poll_event(&ev);
+		if (t == -1)
+		{
+			tb_shutdown();
+			printf("termbox failed to poll an event\n");
+			exit(-1);
+		}
+
+		switch (t) {
+			case TB_EVENT_KEY:
+				if (ev.key = TB_KEY_ESC)
+				{
+					tb_shutdown();
+					exit(0);
+				}
+				break;
+			default:
+				break;
+		}
+	}
+}
